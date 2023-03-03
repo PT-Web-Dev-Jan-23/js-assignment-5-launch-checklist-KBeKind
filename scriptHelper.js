9063f6ae79063c054ca53df6ea1d8adafb319fba
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-03 10:23:39 -0600

    fixed code in myFetch function  i had an extra .then() at the wrong spot

--- scriptHelper.js
+++ scriptHelper.js
@@ -106,13 +106,13 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
 async function myFetch() {
     let planetsReturned;
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
-        return response.json().then(function (json) {
-            return json;
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json")
+    .then(function (response) {
+        return response.json();
         });
-    });
+    
     return planetsReturned;
-}
+ }
 
 function pickPlanet(planets) {
     let planetIndex = Math.floor(Math.random() * planets.length);
