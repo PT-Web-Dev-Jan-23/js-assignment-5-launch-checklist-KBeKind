03eb1d5245f4a03fc4ac8f8fef77582864e7d963
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-03 10:10:29 -0600

    fixed small error

--- scriptHelper.js
+++ scriptHelper.js
@@ -19,7 +19,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput = "") {
-    if (testInput.trim() === "") {
+    if (testInput === "") {
         return "Empty";
     }
     if (isNaN(testInput)) {
