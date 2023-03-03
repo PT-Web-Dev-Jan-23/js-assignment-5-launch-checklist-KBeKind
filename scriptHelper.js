1e07f3d44a615fc026c3eb50f7a4d2657abaad70
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-03 10:32:58 -0600

    added const fetch = require... to top of scripthelper to attempt test pass

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,6 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
+const fetch = require("node-fetch");
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
